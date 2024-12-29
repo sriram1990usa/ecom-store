6bc454bf7f842d273331ab0f78d614b86ea554e4
sriram <sriram1990usa@gmail.com>
2024-12-29 17:34:45 +0530

    update files

---
+++ store/models/customer.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    firstname=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    phone=models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=15)
+
+    def register(self):
+        self.save()
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email=email)
+        except:
+            return False
+
+    def isExists(self):
+        if Customer.objects.filter(email=self.email):
+            return True
+
+        return False
